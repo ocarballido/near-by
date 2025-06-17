5fc1a364422fc092446d384d6bf11a5a361fd18d
ocarballido <ocarballido@gmail.com>
2025-06-17 17:04:45 +0200

    cookie consent layout fixed

--- src/components/Cookies.tsx
+++ src/components/Cookies.tsx
@@ -55,59 +55,57 @@ const CookieConsent = () => {
 
 	return (
 		<div className="font-body fixed bottom-3 left-3 right-3 bg-white rounded-md shadow-md z-50 transform transition-transform duration-500 ease-in-out">
-			<div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
-				<div className="flex flex-col sm:flex-row items-center justify-between gap-4">
-					<div className="flex items-center gap-1 flex-shrink-0">
-						<div className="space-y-1 text-sm text-gray-500 font-medium">
-							<p className="">{t('Cookie banner')}</p>
-							<p>
-								{t.rich('Lee nuestra', {
-									privacyPolicy: (chunks) => (
-										<Link
-											className="underline text-primary-500"
-											href="/legal/privacy"
-										>
-											{chunks}
-										</Link>
-									),
-									conditions: (chunks) => (
-										<Link
-											className="underline text-primary-500"
-											href="/legal/conditions"
-										>
-											{chunks}
-										</Link>
-									),
-									content: (chunks) => (
-										<Link
-											className="underline text-primary-500"
-											href="/legal/content"
-										>
-											{chunks}
-										</Link>
-									),
-								})}
-							</p>
-						</div>
+			<div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between gap-4">
+				<div className="">
+					<div className="space-y-1 text-sm text-gray-500 font-medium">
+						<p className="">{t('Cookie banner')}</p>
+						<p>
+							{t.rich('Lee nuestra', {
+								privacyPolicy: (chunks) => (
+									<Link
+										className="underline text-primary-500"
+										href="/legal/privacy"
+									>
+										{chunks}
+									</Link>
+								),
+								conditions: (chunks) => (
+									<Link
+										className="underline text-primary-500"
+										href="/legal/conditions"
+									>
+										{chunks}
+									</Link>
+								),
+								content: (chunks) => (
+									<Link
+										className="underline text-primary-500"
+										href="/legal/content"
+									>
+										{chunks}
+									</Link>
+								),
+							})}
+						</p>
 					</div>
-					<div className="flex items-center gap-1 flex-shrink-0">
-						<Button
-							color="secondary"
-							label={t('Rechazar')}
-							onClick={handleDecline}
-							className="text-gray-600 hover:text-gray-700"
-						/>
-						<Button
-							label={t('Aceptar')}
-							onClick={handleAccept}
-							className="bg-blue-600 text-white hover:bg-blue-700"
-						/>
-						<ButtonIcon
-							icon={<IconClose />}
-							onClick={handleDecline}
-							aria-label="Close"
-						/>
-					</div>
+				</div>
+				<div className="flex items-center gap-1 flex-shrink-0">
+					<Button
+						color="secondary"
+						label={t('Rechazar')}
+						onClick={handleDecline}
+						className="text-gray-600 hover:text-gray-700"
+					/>
+					<Button
+						label={t('Aceptar')}
+						onClick={handleAccept}
+						className="bg-blue-600 text-white hover:bg-blue-700"
+					/>
+					<ButtonIcon
+						icon={<IconClose />}
+						onClick={handleDecline}
+						aria-label="Close"
+					/>
 				</div>
 			</div>
 		</div>
