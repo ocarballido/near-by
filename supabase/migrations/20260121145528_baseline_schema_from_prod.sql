2a92d5534df2a7b4b21331b94a5713cee88928c5
Oscar Carballido <ocarballido@gmail.com>
2026-03-28 18:36:48 +0100

    migrations added

--- supabase/migrations/20260121145528_baseline_schema_from_prod.sql
+++ supabase/migrations/20260121145528_baseline_schema_from_prod.sql
@@ -10,6 +10,8 @@ create schema if not exists extensions;
 
 create extension if not exists "pgcrypto" with schema extensions;
 create extension if not exists "uuid-ossp" with schema extensions;
+create extension if not exists pg_cron;
+create extension if not exists pg_net;
 
 -- ---------------------------------------------------------
 -- Production schema dump (schema.sql)
