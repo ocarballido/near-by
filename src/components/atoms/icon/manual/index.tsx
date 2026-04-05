74b24155c8b7edbde5ea3364748c3f6498491c42
Oscar Carballido <ocarballido@gmail.com>
2026-04-05 15:01:11 +0200

    new icon components added

---
+++ src/components/atoms/icon/manual/index.tsx
@@ -0,0 +1,58 @@
+import clsx from 'clsx';
+
+import styles from '../styles.module.css';
+
+type IconProps = {
+	size?: number;
+	color?:
+		| 'primary'
+		| 'secondary'
+		| 'success'
+		| 'warning'
+		| 'info'
+		| 'error'
+		| 'body'
+		| 'light'
+		| 'white';
+};
+
+const IconManual = ({ color = 'body', size = 24 }: IconProps) => {
+	const iconStyles = clsx({
+		[styles['icon--primary']]: color === 'primary',
+		[styles['icon--secondary']]: color === 'secondary',
+		[styles['icon--success']]: color === 'success',
+		[styles['icon--warning']]: color === 'warning',
+		[styles['icon--info']]: color === 'info',
+		[styles['icon--error']]: color === 'error',
+		[styles['icon--body']]: color === 'body',
+		[styles['icon--light']]: color === 'light',
+		[styles['icon--white']]: color === 'white',
+	});
+
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			className={iconStyles}
+		>
+			<mask
+				id="mask0_3_666"
+				maskUnits="userSpaceOnUse"
+				x="0"
+				y="0"
+				width={size}
+				height={size}
+			>
+				<rect width={size} height={size} fill="#D9D9D9" />
+			</mask>
+			<g mask="url(#mask0_845_10213)">
+				<path d="M7 16H9V4H7V16ZM6 22C5.16667 22 4.45833 21.7083 3.875 21.125C3.29167 20.5417 3 19.8333 3 19V5C3 4.16667 3.29167 3.45833 3.875 2.875C4.45833 2.29167 5.16667 2 6 2H17V18H6C5.71667 18 5.47917 18.0958 5.2875 18.2875C5.09583 18.4792 5 18.7167 5 19C5 19.2833 5.09583 19.5208 5.2875 19.7125C5.47917 19.9042 5.71667 20 6 20H19V4H21V22H6Z" />
+			</g>
+		</svg>
+	);
+};
+
+export default IconManual;
