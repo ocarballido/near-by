da6401195041da903ffe1b4d0f8a0c94076a60b1
Oscar Carballido <ocarballido@gmail.com>
2026-09-04 12:10:49 +0200

    first section added

--- src/components/molecules/button-link/index.tsx
+++ src/components/molecules/button-link/index.tsx
@@ -1,105 +1,114 @@
-'use client';
+"use client";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 type ButtonLinkProps = {
-	active?: boolean;
-	className?: string;
-	color?: 'primary' | 'secondary' | 'error' | 'white';
-	target?: '_blank' | '_self' | '_parent' | '_top';
-	href: string;
-	iconLeft?: React.ReactNode;
-	iconRight?: React.ReactNode;
-	label: string;
-	download?: string;
-	size?: 'sm' | 'md';
+    active?: boolean;
+    className?: string;
+    color?: "primary" | "secondary" | "error" | "white";
+    target?: "_blank" | "_self" | "_parent" | "_top";
+    href: string;
+    iconLeft?: React.ReactNode;
+    iconRight?: React.ReactNode;
+    label: string;
+    download?: string;
+    size?: "sm" | "md" | "lg";
 };
 
 const ButtonLink = ({
-	active = false,
-	className = '',
-	color = 'primary',
-	href,
-	iconLeft,
-	iconRight,
-	label,
-	target = '_self',
-	download,
-	size = 'md',
+    active = false,
+    className = "",
+    color = "primary",
+    href,
+    iconLeft,
+    iconRight,
+    label,
+    target = "_self",
+    download,
+    size = "md",
 }: ButtonLinkProps) => {
-	const buttonLinkStyles = clsx(
-		{
-			[styles['icon--primary']]:
-				(iconLeft || iconRight) && color === 'white',
-			[styles['icon--secondary']]:
-				(iconLeft || iconRight) && color === 'secondary',
-			[styles['icon--white']]:
-				((iconLeft || iconRight) && color === 'primary') ||
-				color === 'error',
-		},
-		{ 'pl-4': iconLeft },
-		{ 'pr-4': iconRight },
-		{
-			'bg-primary-500 hover:bg-primary-600 focus:bg-primary-600 text-white':
-				color === 'primary',
-		},
-		{
-			'bg-primary-600': color === 'primary' && active,
-		},
-		{
-			'bg-secondary-200 hover:bg-secondary-300 focus:bg-secondary-300 text-gray-800':
-				color === 'secondary',
-		},
-		{
-			'bg-secondary-300': color === 'secondary' && active,
-		},
-		{
-			'bg-error-500 hover:bg-error-600 focus:bg-secondary-300 text-white':
-				color === 'error',
-		},
-		{
-			'bg-error-600': color === 'error' && active,
-		},
-		{
-			'bg-white hover:bg-primary-100 text-primary-500':
-				color === 'white' && !active,
-		},
-		{
-			'bg-primary-100 hover:bg-primary-100 text-primary-500':
-				color === 'white' && active,
-		},
-	);
+    const buttonLinkStyles = clsx(
+        {
+            [styles["icon--primary"]]:
+                (iconLeft || iconRight) && color === "white",
+            [styles["icon--secondary"]]:
+                (iconLeft || iconRight) && color === "secondary",
+            [styles["icon--white"]]:
+                ((iconLeft || iconRight) && color === "primary") ||
+                color === "error",
+        },
+        { "pl-4": iconLeft },
+        { "pr-4": iconRight },
+        {
+            "bg-primary-500 hover:bg-primary-600 focus:bg-primary-600 text-white":
+                color === "primary",
+        },
+        {
+            "bg-primary-600": color === "primary" && active,
+        },
+        {
+            "bg-secondary-200 hover:bg-secondary-300 focus:bg-secondary-300 text-gray-800":
+                color === "secondary",
+        },
+        {
+            "bg-secondary-300": color === "secondary" && active,
+        },
+        {
+            "bg-error-500 hover:bg-error-600 focus:bg-secondary-300 text-white":
+                color === "error",
+        },
+        {
+            "bg-error-600": color === "error" && active,
+        },
+        {
+            "bg-white hover:bg-primary-100 text-primary-500":
+                color === "white" && !active,
+        },
+        {
+            "bg-primary-100 hover:bg-primary-100 text-primary-500":
+                color === "white" && active,
+        },
+        {
+            "py-1 px-5": size === "sm",
+        },
+        {
+            "py-2 px-5": size === "md",
+        },
+        {
+            "py-4 px-6": size === "lg",
+        },
+    );
 
-	const sharedClassName = `font-medium text-base rounded-full transition-all flex items-center justify-center gap-1 ${size === 'sm' ? 'py-1' : 'py-2'} px-5 hover:cursor-pointer disabled:opacity-30 disabled:pointer-events-none ${buttonLinkStyles} ${className}`;
+    const sharedClassName = `font-medium text-base rounded-full transition-all flex items-center justify-center gap-1 hover:cursor-pointer disabled:opacity-30 disabled:pointer-events-none ${buttonLinkStyles} ${className}`;
 
-	if (download) {
-		return (
-			<a
-				className={sharedClassName}
-				href={href}
-				target={target}
-				download={download}
-			>
-				{iconLeft}
-				{label}
-				{iconRight}
-			</a>
-		);
-	}
+    if (download) {
+        return (
+            <a
+                className={sharedClassName}
+                href={href}
+                target={target}
+                download={download}
+            >
+                {iconLeft}
+                {label}
+                {iconRight}
+            </a>
+        );
+    }
 
-	return (
-		<Link className={sharedClassName} href={href} target={target}>
-			{iconLeft}
+    return (
+        <Link className={sharedClassName} href={href} target={target}>
+            {iconLeft}
 
-			{label}
+            {label}
 
-			{iconRight}
-		</Link>
-	);
+            {iconRight}
+        </Link>
+    );
 };
 
 export default ButtonLink;
